f3edaa65cdc10b349e49de3cbc6747a5d43cf7ae
gjmillan <gabrielmillan2023@u.northwestern.edu>
2023-02-27 20:38:31 -0600

    Pushing Real Estate Crypto Wallet

---
+++ RealEstate_CryptoWallet.py
@@ -0,0 +1,81 @@
+# Real Estate
+################################################################################
+
+# This file contains the Ethereum transaction functions that was created for investing in Real Estate Property.
+# By using import statements, you will integrate this `RealEstate_CryptoWallet.py` Python script into the InvestRealEstate_Crypto
+# The InvestRealEstate_Crypto Interface program that is found in the `XXXXX` file.
+
+################################################################################
+# Imports
+
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+from bip44 import Wallet
+from web3 import Account
+from web3 import middleware
+from web3.gas_strategies.time_based import medium_gas_price_strategy
+
+################################################################################
+# Wallet functionality
+
+
+def generate_account():
+    """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
+    # Fetch mnemonic from environment variable.
+    mnemonic = os.getenv("MNEMONIC")
+
+    # Create Wallet Object
+    wallet = Wallet(mnemonic)
+
+    # Derive Ethereum Private Key
+    private, public = wallet.derive_account("eth")
+
+    # Convert private key into an Ethereum account
+    account = Account.privateKeyToAccount(private)
+
+    return account
+
+def get_balance(w3, address):
+    """Using an Ethereum account address access the balance of Ether"""
+   
+    # Get balance of address in Wei
+    wei_balance = w3.eth.get_balance(address)
+
+    # Convert Wei value to ether
+    ether = w3.fromWei(wei_balance, "ether")
+
+    # Return the value in ether
+    return ether
+
+def send_transaction(w3, account, to, wage):
+    """Send an authorized transaction to the Ganache blockchain."""
+    
+    # Set gas price strategy
+    w3.eth.setGasPriceStrategy(medium_gas_price_strategy)
+
+    # Convert eth amount to Wei
+    value = w3.toWei(wage, "ether")
+
+    # Calculate gas estimate
+    gasEstimate = w3.eth.estimateGas(
+        {"to": to, "from": account.address, "value": value}
+    )
+
+    # Construct a raw transaction
+    raw_tx = {
+        "to": to,
+        "from": account.address,
+        "value": value,
+        "gas": gasEstimate,
+        "gasPrice": 0,
+        "nonce": w3.eth.getTransactionCount(account.address),
+    }
+
+    # Sign the raw transaction with ethereum account
+    signed_tx = account.signTransaction(raw_tx)
+
+    # Send the signed transactions
+    return w3.eth.sendRawTransaction(signed_tx.rawTransaction)
